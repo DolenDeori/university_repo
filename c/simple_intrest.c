2fa138bbaf562f9d4822c75fca4df1f70d8046a7
Dolen Ch. Deori <dulench.deori564@gmail.com>
2021-11-11 14:01:31 +0530

    solved the simple intrest program

--- c/simple_intrest.c
+++ c/simple_intrest.c
@@ -6,12 +6,12 @@ I = p*r*t/100
 #include<stdio.h>
 int main(){
     float I, P , R , T;
-    printf("Enter the principle");
+    printf("Enter the principle : ");
     scanf("%f" , &P);
-    printf("Enter the rate of intrest");
+    printf("Enter the rate of intrest : ");
     scanf("%f" , &R);
-    printf("Enter the Time");
+    printf("Enter the Time : ");
     scanf("%f" , &T);
-    I = P*R*I/100;
+    I = (P*R*T)/100;
     printf("The value is : %f" , I);
 }
