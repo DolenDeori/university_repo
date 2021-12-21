97c85d0070c7dd90245024025ab07417e961d302
DolenDeori <dulench.deori564@gmail.com>
2021-12-21 11:24:14 +0530

    added more programs

---
+++ classwork/guess_the_number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    // Generate a random number
+    int secrate_number = rand();
+    printf("%d", secrate_number);
+};
+
+int check_the_number(int num){
+    
+}
